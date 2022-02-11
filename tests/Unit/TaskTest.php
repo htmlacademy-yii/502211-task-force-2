7d3a2a213453537c298470d3309a69e11bd42af6
Poly Titova <poly_titova@mail.ru>
2022-02-11 13:22:22 +0300

    Добавили ещё тестов

--- tests/Unit/TaskTest.php
+++ tests/Unit/TaskTest.php
@@ -11,19 +11,25 @@ class TaskTest extends TestCase
 {
     public function testGetStatusAfterAction()
     {
-        $task = new Task(Task::STATUS_NEW, 1);
+        $customerId = 1;
+
+        $task = new Task(Task::STATUS_NEW, $customerId);
         $status = $task->getStatusAfterAction(Task::ACTION_CANCEL);
         $this->assertEquals(Task::STATUS_CANCELED, $status);
 
-        $task = new Task(Task::STATUS_NEW, 1);
+        $task = new Task(Task::STATUS_NEW, $customerId);
+        $status = $task->getStatusAfterAction(Task::ACTION_RESPOND);
+        $this->assertEquals(Task::ACTION_START, $status);
+
+        $task = new Task(Task::STATUS_NEW, $customerId);
         $status = $task->getStatusAfterAction(Task::ACTION_START);
         $this->assertEquals(Task::STATUS_IN_WORK, $status);
 
-        $task = new Task(Task::STATUS_IN_WORK, 1);
+        $task = new Task(Task::STATUS_IN_WORK, $customerId);
         $status = $task->getStatusAfterAction(Task::ACTION_DONE);
         $this->assertEquals(Task::STATUS_DONE, $status);
 
-        $task = new Task(Task::STATUS_IN_WORK, 1);
+        $task = new Task(Task::STATUS_IN_WORK, $customerId);
         $status = $task->getStatusAfterAction(Task::ACTION_REFUSE);
         $this->assertEquals(Task::STATUS_FAILED, $status);
     }
@@ -42,12 +48,52 @@ class TaskTest extends TestCase
         $actions = $task->getAvailableActions($randomUserId);
         $this->assertEquals([Task::ACTION_RESPOND], $actions);
 
-        $task = new Task(Task::STATUS_IN_WORK, $customerId);
+
+        $task = new Task(Task::STATUS_CANCELED, $customerId);
+        $actions = $task->getAvailableActions($customerId);
+        $this->assertEquals([], $actions);
+
+        $task = new Task(Task::STATUS_CANCELED, $customerId);
+        $actions = $task->getAvailableActions($randomUserId);
+        $this->assertEquals([], $actions);
+
+
+        $task = new Task(Task::STATUS_IN_WORK, $customerId, $executorId);
         $actions = $task->getAvailableActions($customerId);
         $this->assertEquals([Task::ACTION_DONE], $actions);
 
         $task = new Task(Task::STATUS_IN_WORK, $customerId, $executorId);
         $actions = $task->getAvailableActions($executorId);
         $this->assertEquals([Task::ACTION_REFUSE], $actions);
+
+        $task = new Task(Task::STATUS_IN_WORK, $customerId, $executorId);
+        $actions = $task->getAvailableActions($randomUserId);
+        $this->assertEquals([], $actions);
+
+
+        $task = new Task(Task::STATUS_DONE, $customerId, $executorId);
+        $actions = $task->getAvailableActions($customerId);
+        $this->assertEquals([], $actions);
+
+        $task = new Task(Task::STATUS_DONE, $customerId, $executorId);
+        $actions = $task->getAvailableActions($executorId);
+        $this->assertEquals([], $actions);
+
+        $task = new Task(Task::STATUS_DONE, $customerId, $executorId);
+        $actions = $task->getAvailableActions($randomUserId);
+        $this->assertEquals([], $actions);
+
+
+        $task = new Task(Task::STATUS_FAILED, $customerId, $executorId);
+        $actions = $task->getAvailableActions($customerId);
+        $this->assertEquals([], $actions);
+
+        $task = new Task(Task::STATUS_FAILED, $customerId, $executorId);
+        $actions = $task->getAvailableActions($executorId);
+        $this->assertEquals([], $actions);
+
+        $task = new Task(Task::STATUS_FAILED, $customerId, $executorId);
+        $actions = $task->getAvailableActions($randomUserId);
+        $this->assertEquals([], $actions);
     }
 }
